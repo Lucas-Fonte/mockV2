5fee0b1c1fafef5cf1479e93f7c73c9cc8c837c6
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-08-04 19:12:41 -0300

    Added content from github profile readme

--- src/layout/About/index.tsx
+++ src/layout/About/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Container, Content } from './styles';
-import Card from '../../components/Card';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ReactMarkdown from 'react-markdown';
+import { Container, Content, MarkdownContent } from './styles';
 import Header from '../../components/Header';
+import Loading from '../../components/Loading';
+import { timeout } from '../../utils/tools/timeout';
 
-const About: React.FC = () => (
-  <Container>
-    <Header />
-    <Content>
-      <Card content="about" />
-    </Content>
-  </Container>
-);
+const README_URL = 'https://raw.githubusercontent.com/Lucas-Fonte/Lucas-Fonte/master/README.md';
+
+const About: React.FC = () => {
+  const [content, setContent] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>();
+
+  useEffect(() => {
+    const loadContent = async () => {
+      const { data } = await axios.get(README_URL);
+
+      setLoading(true);
+      await timeout(1500);
+      setContent(data);
+      setLoading(false);
+    };
+
+    loadContent();
+  }, [content]);
+
+  return (
+    <Container>
+      <Header />
+      <Content>
+        {loading
+          ? <Loading loadingType="loading" />
+          : (
+            <MarkdownContent>
+              <ReactMarkdown source={content} escapeHtml={false} />
+            </MarkdownContent>
+          )}
+      </Content>
+    </Container>
+  );
+};
 
 export { About };
--- src/layout/About/styles.ts
+++ src/layout/About/styles.ts
@@ -8,8 +8,53 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 10%;
-  width: 100%;
+  align-items: center;
   height: 100%;
   
+`;
+
+export const MarkdownContent = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: left;
+  flex-direction: column;
+  width: 80%;  
+  height: 100%;
+  overflow: auto;
+  padding-top: 15vh;
+  padding-bottom: 15vh;
+
+  @media (min-width: 600px){
+    width: 40%; 
+  }
+
+  h3:first-of-type {
+    font-size: 30px;
+  }
+
+  h2 {
+    margin-top: 14px;
+    margin-bottom: 14px;
+  }
+
+  hr {
+    display: none;
+    margin-top: 14px;
+    margin-bottom: 14px;
+  }
+
+  ul {
+    margin-bottom: 14px;
+  }
+
+  #connectList {
+    margin-top: 10px;
+  }
+
+  #toolsList {
+      margin-top: 10px;
+      overflow-wrap: break-word;
+      width: 300px;
+  }
+
 `;
