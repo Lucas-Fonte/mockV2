f75596284e34052ec701066357623b5de0928488
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-09-01 19:16:28 -0300

    Updated main anchor

--- src/layout/Main/styles.ts
+++ src/layout/Main/styles.ts
@@ -8,22 +8,25 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
-  
 `;
 
 export const ProjectsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 95%;
-  max-width: 600px;
+  align-items: center;
+  max-width: 95%;
   padding-left: 5%;
   overflow: auto;
 
+  a {
+    max-width: 400px;
+  }
+
   img {
+    max-width: 95%;
     margin-bottom: 10px;
-    width: 95%;
-    max-width: 600px;
   }
 `;
